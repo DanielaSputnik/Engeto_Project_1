2ce5806af47cf24808b5267018cb352f7ecfa2b3
DanielaSputnik <68498740+DanielaSputnik@users.noreply.github.com>
2020-09-25 23:53:57 +0200

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -1,7 +1,6 @@
 '''
 author =
 '''
-from unicodedata import numeric
 
 TEXT = ['''
 'Situated about 10 miles west of Kemmerer, 
@@ -41,50 +40,38 @@ credentials = {
 
 
 def main():
-    print("GREETINGS! WELCOME TO THE".center(50))
-    print("|T|E|X|T| |A|N|A|L|Y|Z|E|R|".center(50))
-    linebreak()
-    credentials_check()
-    linebreak()
-    print(f"WE HAVE {len(TEXT)} TEXTS FOR ANALYSIS.".center(50))
-    linebreak()
-
-    recursion = True
-    while recursion:
-        text_number = text_selection()
-        if text_number > len(TEXT):
-            print(f'Selected text is unavailable, please select text number 1 - {len(TEXT)}.')
+    print_greetings()
+    run_credentials_check()
+    print_text_options(TEXT)
+    analysis_ongoing = True
+    while analysis_ongoing:
+        analyzed_txt_number = user_txt_selection()
+        if analyzed_txt_number > len(TEXT):
+            print_txt_unavailable(TEXT)
             continue
-        selected_text = TEXT[text_number - 1]
-        text_printing(selected_text)
-        clean_txt = text_preparation(selected_text)
-        print(f'''TEXT {text_number} CONTAINS:
-                  WORDS  |  {text_statistics(clean_txt)[0]}
-        TITLECASE WORDS  |  {text_statistics(clean_txt)[1]}
-        UPPERCASE WORDS  |  {text_statistics(clean_txt)[2]}
-        LOWERCASE WORDS  |  {text_statistics(clean_txt)[3]}
-        NUMERIC STRINGS  |  {text_statistics(clean_txt)[4]}'''
-              )
-        linebreak()
-        print(f"WORD LENGTH FREQUENCIES:")
-        bar_chart(clean_txt)
-        linebreak()
-        print(f"SUM OF ALL THE NUMBERS IN SELECTED TEXT IS:", numeric_sum(clean_txt))
-        linebreak()
+        analyzed_txt = TEXT[analyzed_txt_number - 1]
+        print_out_text(analyzed_txt)
+        clean_txt = txt_preparation(analyzed_txt)
+        print_txt_statistics(analyzed_txt_number, clean_txt)
+        print_bar_chart(clean_txt)
+        print_numeric_sum(clean_txt)
 
-        recursion_check = input('Do you wish to select another text? [Y/N]').lower()
-        if recursion_check == 'y':
-            recursion = True
-        else:
-            print(f"GOODBYE!".center(50))
-            recursion = False
+        if select_diff_text() != 'y':
+            analysis_ongoing = False
+            print_goodbye()
 
 
-def linebreak():
+def linebreak() -> str:
     print('=' * 50)
 
 
-def credentials_check():
+def print_greetings() -> str:
+    print("GREETINGS! WELCOME TO THE".center(50))
+    print("|T|E|X|T| |A|N|A|L|Y|Z|E|R|".center(50))
+    linebreak()
+
+
+def run_credentials_check() -> None:
     checking = True
     while checking:
         user_name = input('Please enter username:').lower()
@@ -97,7 +84,13 @@ def credentials_check():
             print(f"WELCOME {user_name.upper()}!".center(50))
 
 
-def text_selection():
+def print_text_options(texts: list) -> str:
+    linebreak()
+    print(f"WE HAVE {len(texts)} TEXTS FOR ANALYSIS.".center(50))
+    linebreak()
+
+
+def user_txt_selection() -> int:
     while True:
         try:
             selected_text = int(input('Select text number you wish to analyze:'))
@@ -108,7 +101,7 @@ def text_selection():
             return selected_text
 
 
-def text_printing(selected_text):
+def print_out_text(selected_text: str) -> str:
     linebreak()
     print("YOUR SELECTED TEXT IS:".center(50))
     linebreak()
@@ -116,22 +109,26 @@ def text_printing(selected_text):
     linebreak()
 
 
-def text_preparation(selected_text):
+def print_txt_unavailable(avail_texts: list) -> str:
+    print(f'Selected text is unavailable, please select text number 1 - {len(avail_texts)}.')
+
+
+def txt_preparation(selected_text: str) -> list:
     txt_split = selected_text.split()
-    clean_txt = []
+    word_list = []
     for word in txt_split:
         clean_word = word.strip(".,")
-        clean_txt.append(clean_word)
-    return clean_txt
+        word_list.append(clean_word)
+    return word_list
 
 
-def text_statistics(cleaned_text):
-    word_count = len(cleaned_text)
+def text_statistics(word_list: list) -> tuple:
+    word_count = len(word_list)
     title_count = 0
     upper_count = 0
     lower_count = 0
     numeric_count = 0
-    for word in cleaned_text:
+    for word in word_list:
         if word.istitle():
             title_count += 1
         elif word.isupper():
@@ -143,25 +140,58 @@ def text_statistics(cleaned_text):
     return word_count, title_count, upper_count, lower_count, numeric_count
 
 
-def bar_chart(clean_txt):
-    clean_txt_copy = clean_txt.copy()
-    word_pop_dictionary = {}
-    while clean_txt_copy:
-        wordpop = clean_txt_copy.pop()
-        if len(wordpop) not in word_pop_dictionary:
-            word_pop_dictionary[len(wordpop)] = 0
-        word_pop_dictionary[len(wordpop)] = word_pop_dictionary[len(wordpop)] + 1
-
-    for i in sorted(word_pop_dictionary):
-        print(i, '*' * word_pop_dictionary[i], word_pop_dictionary[i])
+def print_txt_statistics(txt_no: int, word_list: list) -> str:
+    print(f'''TEXT {txt_no} CONTAINS:
+              WORDS  |  {text_statistics(word_list)[0]}
+    TITLECASE WORDS  |  {text_statistics(word_list)[1]}
+    UPPERCASE WORDS  |  {text_statistics(word_list)[2]}
+    LOWERCASE WORDS  |  {text_statistics(word_list)[3]}
+    NUMERIC STRINGS  |  {text_statistics(word_list)[4]}'''
+          )
 
 
-def numeric_sum(txt_split):
+def bar_chart(word_list: list) -> str:
+    word_list_copy = word_list.copy()
+    word_dictionary = {}
+    while word_list_copy:
+        wordpop = word_list_copy.pop()
+        if len(wordpop) not in word_dictionary:
+            word_dictionary[len(wordpop)] = 0
+        word_dictionary[len(wordpop)] = word_dictionary[len(wordpop)] + 1
+
+    for i in sorted(word_dictionary):
+        print(i, '*' * word_dictionary[i], word_dictionary[i])
+
+
+def print_bar_chart(word_list: list) -> str:
+    linebreak()
+    print(f"WORD LENGTH FREQUENCIES:")
+    bar_chart(word_list)
+
+
+def numeric_sum(word_list: list) -> int:
     numeric_calc = []
-    for word in txt_split:
+    for word in word_list:
         if word.isnumeric():
             numeric_calc.append(int(word))
     return sum(numeric_calc)
+
+
+def print_numeric_sum(word_list: list) -> str:
+    linebreak()
+    print(f"SUM OF ALL THE NUMBERS IN SELECTED TEXT IS:", numeric_sum(word_list))
+    linebreak()
+
+
+def select_diff_text() -> bool:
+    check = input('Do you wish to select another text? [Y/N]').lower()
+    return check
+
+
+def print_goodbye() -> str:
+    linebreak()
+    print(f"GOODBYE!".center(50))
+    linebreak()
 
 
 main()
